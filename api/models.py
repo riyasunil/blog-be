a1c648e57846296bb50affce8cd440defa653027
riyasunil <riaelisabeths@gmail.com>
2024-12-12 17:52:20 +0530

    added tag system

--- api/models.py
+++ api/models.py
@@ -4,11 +4,21 @@ from ckeditor.fields import RichTextField
 
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
 class Blog(models.Model):
     title = models.CharField(max_length=250, unique=True)
     content = RichTextField()
     created_at = models.DateField(auto_now_add=True)
     slug = models.SlugField(unique=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    
 
     def save(self, *args, **kwargs):
         if not self.slug:
--- api/views.py
+++ api/views.py
@@ -1,21 +1,28 @@
 from rest_framework import generics
-from .models import Blog
+from .models import Blog, Tag
 from .serializers import BlogSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated , IsAdminUser
 from rest_framework.exceptions import NotFound
 from markdown import markdown
 from django.shortcuts import render, get_object_or_404
 
-
-
-
 # Create your views here.
-
-
 class BlogListView(generics.ListAPIView):
     serializer_class = BlogSerializer
     queryset = Blog.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnly] 
+
+class BlogByTagView(generics.ListAPIView):
+    serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly] 
+    def get_queryset(self):
+        tag_name = self.kwargs.get('tag')
+        try:
+            tag = Tag.objects.get(name=tag_name)
+            return Blog.objects.filter(tags__name=tag)
+        except Tag.DoesNotExist:
+            return Blog.objects.none()
+
 
 class BlogDetailView(generics.RetrieveAPIView):
     serializer_class = BlogSerializer
