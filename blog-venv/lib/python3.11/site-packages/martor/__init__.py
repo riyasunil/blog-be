25f5e542499f9c9084b6bbc1a9fa129101a72fff
riyasunil <mdl21cs102@mec.ac.in>
2025-01-09 12:43:07 +0000

    imsotiredofthis

--- blog-venv/lib/python3.11/site-packages/martor/__init__.py
+++
@@ -1,4 +0,0 @@
-__VERSION__ = "1.6.45"
-__RELEASE_DATE__ = "15-Nov-2024"
-__AUTHOR__ = "Agus Makmun (Summon Agus)"
-__AUTHOR_EMAIL__ = "summon.agus@gmail.com"
--- blog-venv/lib/python3.11/site-packages/martor/tests/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-
-from .views import TestFormView
-
-urlpatterns = [
-    path("test-form-view/", TestFormView.as_view()),
-    path("martor/", include("martor.urls")),
-]
